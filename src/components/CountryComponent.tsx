f0ce7d89b030fc6f8a5c3bee255aa2b40ea33947
MaNoLiN-CRV <manoliin.27@gmail.com>
2024-12-10 16:20:26 +0100

    Basic structure creation in process

---
+++ src/components/CountryComponent.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function CountryComponent() {
+  return (
+    <View>
+      <Text>CountryComponent</Text>
+    </View>
+  )
+}
